43b4ec2f1d0a3343cb1a6dbfbd11a680192018ba
klapauciuz <synk.pit@gmail.com>
2025-01-01 02:00:23 +0400

    ok

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-@a pp.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
